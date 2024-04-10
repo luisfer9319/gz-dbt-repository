4841d91b37dbad44a1302bcc0a85cb1e14315cbd
luisfersh9319@gmail.com <luisfersh9319@gmail.com>
2024-04-10 09:53:07 +0000

    correction

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,4 +1,5 @@
 SELECT 
+
     products_id, 
     date_date, 
     orders_id,
@@ -7,6 +8,7 @@ SELECT
     CAST(purchase_price AS FLOAT64), 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    
 FROM {{ref("stg_data__sales")}} s
 LEFT JOIN {{ref("stg_data__product")}} p 
     USING (products_id)
